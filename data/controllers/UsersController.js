706cf83d7731874f8bf901cbf7c3296c78c4efe9
centime <quelques.centimes@gmail.com>
2014-04-10 10:27:29 +0200

    bugged onDataAvailable

--- data/controllers/UsersController.js
+++ data/controllers/UsersController.js
@@ -1,4 +1,7 @@
 
 app.controller('UsersController', function($scope){
+    self.port.on("names", function (channel,nicks) {
+            console.log(nicks)
+        });
     $scope.users = ['NoOne','Really'];
 });
