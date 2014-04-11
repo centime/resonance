f6fce84bb80f4786f364177c0bd5e8b627be5fb8
centime <quelques.centimes@gmail.com>
2014-04-11 14:46:06 +0200

    new layout using flex, controls panel, features panel, features button, Comments, clear input, check newMsg

--- data/controllers/IrcController.js
+++ data/controllers/IrcController.js
@@ -1,8 +1,11 @@
 //TODO
+// this is the ugly global var !
 var IRC = {} ;
+// get the chan from the background script
 self.port.on('chan',function(c){
     IRC.chan = c ;
 });
+// get the nick from the background script
 self.port.on('nick',function(n){
     IRC.nick = n ;
 });
--- data/controllers/MessagesController.js
+++ data/controllers/MessagesController.js
@@ -1,19 +1,27 @@
-
 app.controller("MessagesController", function($scope){
-    
+    // List of every messages that has been sent or received in the current channel (page).
     $scope.messages = [];
     $scope.newMessage = '';
-    var elmt = angular.element('#messages .list') ;
-    
+    // Select the DOM element for messages.
+    var elmt = angular.element('messages > ul') ;
+    // Send a new message.
     $scope.submitNewMessage =  function(){
-                            self.port.emit('say',IRC.chan, $scope.newMessage);
+                            var msg = $scope.newMessage ;
+                            // Tell the background script to 'say' 'msg' on channel 'IRC.chan'
+                            if (msg !== '')
+                                self.port.emit('say',IRC.chan, msg);
+                            // Clear the input.
+                            $scope.newMessage = '';
                         };
-
+    // Listen for a 'message' event.
     self.port.on("message", function (from,to,message) {
+        // The line will be 'User : Message'.
         var text = from+' : '+message;
+        // Append it to the list of all messages.
         $scope.messages.push(text)
+        // Update the view.
         $scope.$apply()
-        // scroll down
+        // Scroll down the view.
         elmt.animate({ scrollTop: elmt.prop('scrollHeight')}, 1000);
         
         
