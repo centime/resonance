7c2c9734e1d61f51d7f6addb8528361884f94b62
centime <quelques.centimes@gmail.com>
2014-04-20 12:54:03 +0200

    testBot

---
+++ tests/testBot.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+var irc = require('./irc');
+
+var bot = new irc.Client('chat.freenode.net', 'Resonance-test', {
+    debug: true,
+    channels: [],
+});
+
+bot.addListener('error', function(message) {
+    console.error('ERROR: %s: %s', message.command, message.args.join(' '));
+});
+
+bot.addListener('pm',function(nick,message){
+    if (message.match(/^join/)){
+        var chan = message.replace('join ','');
+        bot.join(chan);
+    } else if (message.match(/^say/)){
+        var chan = message.replace('say ','').split(' ')[0];
+        var message = message.replace('say ','').replace(chan+' ','');
+        bot.say(chan,message);
+    };
+});
